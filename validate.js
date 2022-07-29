bc30f7f5893fec210dea461536adf8f090ca5ad7
Felipe Lúcio <felipesou94@gmail.com>
2022-07-29 15:47:57 -0300

    Corrigindo alguns estilos

--- validate.js
+++ validate.js
@@ -1,1 +1,15 @@
-//Seu JavaScript de validação aqui
+const button = document.querySelector('.formcontato__botao');
+const nome = document.querySelector('#nome');
+const email = document.querySelector('#email');
+const assunto = document.querySelector('#assunto');
+const texto = document.querySelector('#mensagem');
+
+button.addEventListener('click', ()=> {
+    if( nome.value !== '' && email.value !== '' && assunto.value !== '' ) {
+        nome.value = '';
+        email.value = '';
+        assunto.value = '';
+        alert('Mensagem enviada com sucesso!');
+    }
+
+})
