a050ae6b4775670bec4f032e4d788746545c2240
Felipe Lúcio <felipesou94@gmail.com>
2022-08-07 14:19:24 -0300

    responsivo finalizado

--- validate.js
+++ validate.js
@@ -5,11 +5,20 @@ const assunto = document.querySelector('#assunto');
 const texto = document.querySelector('#mensagem');
 
 button.addEventListener('click', ()=> {
-    if( nome.value !== '' && email.value !== '' && assunto.value !== '' ) {
+    if( nome.value !== '' && assunto.value !== '' && email.value !== '' ) {
+        if(validEmail(email.value)){
         nome.value = '';
         email.value = '';
         assunto.value = '';
+        texto.value = '';
         alert('Mensagem enviada com sucesso!');
+        }else{
+            alert('Email inválido!');
+        }
     }
-
+   
 })
+
+function validEmail(email){
+    return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email)
+}
